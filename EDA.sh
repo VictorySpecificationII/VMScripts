4f6c27da442b73c95c6f7bbb2861d644915f48c1
Antreas Christofi <victoryspecificationii@gmail.com>
2020-03-02 18:54:06 +0200

    EDA launch script

---
+++ EDA.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# Bash Menu Script Example
+echo "===================EDA Launch Script=================="
+
+
+PS3='Please enter your choice: '
+options=("Quartus" "Vivado" "VSDFlow" "iVerilog" "Verilator" "MAGIC" "OpenTimer" "ngSpice" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Quartus")
+            echo "Launching Quartus 19.1..."
+	    sleep 2s
+	    cd /tools/Intel/intelFPGA_lite/19.1/quartus/bin/
+	    sudo ./quartus --64bit&
+            ;;
+        "Vivado")
+            echo "Launching Vivado ISE 18.3..."
+	    sleep 2s
+	    cd /tools/Xilinx/Vivado/2018.3/bin/
+	    sudo ./vivado
+            ;;
+        "VSDFlow")
+            echo "Redirecting to VSDFlow environment..."
+	    cd /home/andrew/Software/vsdflow
+	    echo "Input path of Verilog file to run on VSDFlow"
+            read pathtoverilogfile
+            sudo ./vsdflow $pathtoverilogfile
+	    ls
+	    break
+            ;;
+        "iVerilog")
+            echo "Running iVerilog..."
+	    echo "Icarus Verilog is a Verilog simulation and synthesis tool. It operates as a compiler, compiling source code written in Verilog (IEEE-1364) into some target format. For batch simulation, the compiler can generate an intermediate form called vvp assembly. This intermediate form is executed by the ``vvp'' command. For synthesis, the compiler generates netlists in the desired format."
+	    echo "Input path of Verilog file to run on iVerilog:"
+            read pathtoverilogfile
+            iverilog $pathtoverilogfile
+	    ls
+	    break
+            ;;
+	"Verilator")
+            echo "Running Verilator v 4.020...\n"
+	    echo "Verilator converts synthesizable (generally not behavioral) Verilog code, plus some Synthesis, SystemVerilog and a small subset of Verilog AMS into C++ or SystemC code. It is not a complete simulator, but a compiler."
+	    sleep 2s
+	    echo "Input path of Verilog file to run on Verilator:"
+            read pathtoverilogfile
+            verilator $pathtoverilogfile
+	    break
+	    ;;
+	"MAGIC")
+            echo "Launching MAGIC VLSI custom layout tool"
+	    sleep 2s
+	    magic&
+            ;;
+	"OpenTimer")
+            echo "Launching OpenTimer STA tool"
+	    sleep 2s
+	    cd /home/andrew/OpenTimerInstallation/bin
+	    ./ot-shell
+            ;;
+	"ngSpice")
+            echo "Launching ngSpice Circuit Simulator tool"
+	    sleep 2s
+	    ngspice
+            ;;
+        "Quit")
+            break
+            ;;
+
+        *) echo "invalid option $REPLY";;
+    esac
+done
