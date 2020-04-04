23f884d8f8066ae5a734747d82cb2136a7c219fd
Antreas Christofi <victoryspecificationii@gmail.com>
2020-04-04 16:38:37 +0300

    Cosmetic changes

--- Motorsport.sh
+++ Motorsport.sh
@@ -1,65 +1,71 @@
 #!/bin/bash
 # Bash Menu Script Example
-echo "===================Motorsport Launch Script=================="
+#echo "===================Motorsport Launch Script=================="
+
+figlet -c "Motorsport Launch Script"
 
 
 PS3='Please enter your choice: '
-options=("GNU Octave" "Jupyter Notebook" "Pycharm 2019.3" "TunerStudio" "MegaLogViewer" "ProjectChrono System Dynamics" "The Open Racing Car Simulator" "Quit")
+options=("GNU Octave" "Jupyter Notebook" "Pycharm 2019.3" "TunerStudio" "MegaLogViewer" "ProjectChrono System Dynamics" "The Open Racing Car Simulator" "ROScore" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "GNU Octave")
             echo "Launching GNU Octave..."
-	    sleep 2s
-	    octave
+	    	sleep 2s
+	    	octave
             ;;
-            "Quit")
-            break
-            ;;
-	"Jupyter Notebook")
+		"Jupyter Notebook")
             echo "Launching Jupyter Notebook..."
-	    sleep 2s
-	    jupyter-notebook
-	    break
+	    	sleep 2s
+	    	jupyter-notebook
+	    	break
             ;;
-    "Pycharm 2019.3")
-        echo "Launching Pycharm 2019.3..."
-	    sleep 2s
-	    pycharm-community&
-	    break
+    	"Pycharm 2019.3")
+        	echo "Launching Pycharm 2019.3..."
+	    	sleep 2s
+	    	pycharm-community&
+	    	break
             ;;
-	"TunerStudio")
+		"TunerStudio")
             echo "Launching TunerStudio..."
-	    sleep 2s
-	    cd /home/andrew/Software/TunerStudioMS
-	    ./TunerStudio.sh
-	    break
+	    	sleep 2s
+	    	cd /home/andrew/Software/TunerStudioMS
+	    	./TunerStudio.sh
+	    	break
             ;;
-	"MegaLogViewer")
+		"MegaLogViewer")
             echo "Launching MegaLogViewer..."
-	    sleep 2s
-	    cd /home/andrew/Software/MegaLogViewerHD
-	    ./MegaLogViewer.sh
-	    break
+	    	sleep 2s
+	    	cd /home/andrew/Software/MegaLogViewerHD
+	    	./MegaLogViewer.sh
+	    	break
             ;;
-	"ProjectChrono System Dynamics")
+		"ProjectChrono System Dynamics")
             echo "Redirecting to documentation..."
-	    sleep 2s
-	    firefox --new-tab http://api.projectchrono.org/tutorial_table_of_content_chrono.html
-	    #echo "Section under active development, check back later..."
-	    break
+	    	sleep 2s
+	    	firefox --new-tab http://api.projectchrono.org/tutorial_table_of_content_chrono.html
+	    	#echo "Section under active development, check back later..."
+	    	break
             ;;
-    "The Open Racing Car Simulator")
+    	"The Open Racing Car Simulator")
             echo "Launching The Open Racing Car Simulator..."
-	    sleep 2s
-	    torcs&
-	    break
+	    	sleep 2s
+	    	torcs&
+	    	break
             ;;
-    
-            "Quit")
+        "ROScore")
+            echo "Launching ROScore..."
+	    	sleep 2s
+	    	roscore
+	    	break
+            ;;
+        "Quit")
+			echo "Goodbye!"
+	    	sleep 1s
             break
             ;;
 
-        *) echo "invalid option $REPLY";;
+        *) echo "$REPLY is an invalid choice! Please input the number of the application you wish to run!";;
     esac
 done
