fe9a217862119486f82bb5945333557569005982
Antreas Christofi <victoryspecificationii@gmail.com>
2020-04-05 19:16:18 +0300

    added code::blocks for ros development

--- Motorsport.sh
+++ Motorsport.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Bash Menu Script Example
-#echo "===================Motorsport Launch Script=================="
 
 figlet -c "Motorsport Launch Script"
 
@@ -14,7 +13,7 @@ echo " "
 
 
 PS3='Please enter your choice: '
-options=("GNU Octave" "Jupyter Notebook" "Pycharm 2019.3" "TunerStudio" "MegaLogViewer" "ProjectChrono System Dynamics" "The Open Racing Car Simulator" "ROScore" "Quit")
+options=("GNU Octave" "Jupyter Notebook" "Pycharm 2019.3" "Code::Blocks" "ROScore" "The Open Racing Car Simulator" "ProjectChrono System Dynamics" "TunerStudio" "MegaLogViewer" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -36,6 +35,31 @@ do
 	    	pycharm-community &>/dev/null &
 	    	break
             ;;
+		"Code::Blocks")
+			echo "Launching Code::Blocks IDE for ROS development..."
+			sleep 2s
+			codeblocks &>/dev/null &
+			break 
+			;;            
+        "ROScore")
+            echo "Launching ROScore..."
+	    	sleep 2s
+	    	roscore
+	    	break
+            ;;
+    	"The Open Racing Car Simulator")
+            echo "Launching The Open Racing Car Simulator v.1.3.7 SCR..."
+	    	sleep 2s
+	    	torcs&
+	    	break
+            ;;
+		"ProjectChrono System Dynamics")
+            echo "Redirecting to documentation..."
+	    	sleep 2s
+	    	firefox --new-tab http://api.projectchrono.org/tutorial_table_of_content_chrono.html
+	    	#echo "Section under active development, check back later..."
+	    	break
+            ;;
 		"TunerStudio")
             echo "Launching TunerStudio..."
 	    	sleep 2s
@@ -47,25 +71,6 @@ do
             echo "Launching MegaLogViewer..."
 	    	sleep 2s
 	    	bash /home/andrew/Software/MegaLogViewerHD/MegaLogViewer.sh &>/dev/null &
-	    	break
-            ;;
-		"ProjectChrono System Dynamics")
-            echo "Redirecting to documentation..."
-	    	sleep 2s
-	    	firefox --new-tab http://api.projectchrono.org/tutorial_table_of_content_chrono.html
-	    	#echo "Section under active development, check back later..."
-	    	break
-            ;;
-    	"The Open Racing Car Simulator")
-            echo "Launching The Open Racing Car Simulator..."
-	    	sleep 2s
-	    	torcs&
-	    	break
-            ;;
-        "ROScore")
-            echo "Launching ROScore..."
-	    	sleep 2s
-	    	roscore
 	    	break
             ;;
         "Quit")
